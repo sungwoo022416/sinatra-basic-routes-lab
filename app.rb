7b4c62f4b7c3817a12b121c645d9ea96e6fcf13f
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-20 00:05:49 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hellow, World!"
+    end
+
+    get ('/name') {"My name is Sungwoo"}
+    
+    get ('/hometown') {"My hometown is Busan"}
+
+    get ('/favorite-song') {"My favorite song is classic"}
 end
